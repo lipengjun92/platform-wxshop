99f35b1e9fcb0310a5fd02acde13e2ae67d7be5d
小哥哥 <939961241@qq.com>
2018-05-22 21:49:31 +0800

    发送验证码使用创瑞云平台

--- platform-api/src/main/java/com/platform/api/ApiUserController.java
+++ platform-api/src/main/java/com/platform/api/ApiUserController.java
@@ -3,16 +3,18 @@ package com.platform.api;
 import com.alibaba.fastjson.JSONObject;
 import com.platform.annotation.IgnoreAuth;
 import com.platform.annotation.LoginUser;
+import com.platform.entity.SmsConfig;
 import com.platform.entity.SmsLogVo;
 import com.platform.entity.UserVo;
 import com.platform.service.ApiUserService;
+import com.platform.service.SysConfigService;
 import com.platform.util.ApiBaseAction;
-import com.platform.utils.CharUtil;
-import com.platform.utils.sms.SmsAlidayu;
+import com.platform.utils.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,6 +28,8 @@ import java.util.Map;
 public class ApiUserController extends ApiBaseAction {
     @Autowired
     private ApiUserService userService;
+    @Autowired
+    private SysConfigService sysConfigService;
 
     /**
      */
@@ -61,15 +65,36 @@ public class ApiUserController extends ApiBaseAction {
         }
         //生成验证码
         String sms_code = CharUtil.getRandomNum(4);
-        String smsTemplateCode = "SMS_94340007";
         String msgContent = "您的验证码是：" + sms_code + "，请在页面中提交验证码完成验证。";
-        String param = "{\"code\":\"" + sms_code + "\"}";
         // 发送短信
 //        String rpt = "0";
-        String rpt = SmsAlidayu.sendTplShortMessage(param, phone, smsTemplateCode);
-        if (rpt == null || rpt.equals("0") == false) {
-            return toResponsFail("短信发送失败");
-        } else {
+        String result = "";
+        //获取云存储配置信息
+        SmsConfig config = sysConfigService.getConfigObject(ConfigConstant.SMS_CONFIG_KEY, SmsConfig.class);
+        if (StringUtils.isNullOrEmpty(config)) {
+            throw new RRException("请先配置短信平台信息");
+        }
+        if (StringUtils.isNullOrEmpty(config.getName())) {
+            throw new RRException("请先配置短信平台用户名");
+        }
+        if (StringUtils.isNullOrEmpty(config.getPwd())) {
+            throw new RRException("请先配置短信平台密钥");
+        }
+        if (StringUtils.isNullOrEmpty(config.getSign())) {
+            throw new RRException("请先配置短信平台签名");
+        }
+        try {
+            /**
+             * 状态,发送编号,无效号码数,成功提交数,黑名单数和消息，无论发送的号码是多少，一个发送请求只返回一个sendid，如果响应的状态不是“0”，则只有状态和消息
+             */
+            result = SmsUtil.crSendSms(config.getName(), config.getPwd(), phone, msgContent, config.getSign(),
+                    DateUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss"), "");
+        } catch (Exception e) {
+
+        }
+        String arr[] = result.split(",");
+
+        if ("0".equals(arr[0])) {
             smsLogVo = new SmsLogVo();
             smsLogVo.setLog_date(System.currentTimeMillis() / 1000);
             smsLogVo.setUser_id(loginUser.getUserId());
@@ -78,6 +103,8 @@ public class ApiUserController extends ApiBaseAction {
             smsLogVo.setSms_text(msgContent);
             userService.saveSmsCodeLog(smsLogVo);
             return toResponsSuccess("短信发送成功");
+        } else {
+            return toResponsFail("短信发送失败");
         }
     }
 
