ba75e385e47aa32ba8e0e88b7c1323bab3097a8f
suzy2 <suzy2@lenovo.com>
2018-07-02 23:14:19 +0800

    拆分首页  把单个请求拆分成多个提高首页加载速度

--- platform-api/src/main/java/com/platform/api/ApiIndexController.java
+++ platform-api/src/main/java/com/platform/api/ApiIndexController.java
@@ -160,4 +160,152 @@ public class ApiIndexController extends ApiBaseAction {
         resultObj.put("categoryList", newCategoryList);
         return toResponsSuccess(resultObj);
     }
+    
+
+    /**
+     * app首页
+     */
+    @ApiOperation(value = "新商品信息")
+    @IgnoreAuth
+    @RequestMapping(value = "newGoods", method = RequestMethod.GET)
+    public Object newGoods() {
+    	Map<String, Object> resultObj = new HashMap<String, Object>();
+    	//
+    	Map<String, Object> param = new HashMap<String, Object>();
+        param.put("is_new", 1);
+        param.put("is_delete", 0);
+        param.put("fields", "id, name, list_pic_url, retail_price");
+        PageHelper.startPage(0, 4,false);
+        List<GoodsVo> newGoods = goodsService.queryList(param);
+        resultObj.put("newGoodsList", newGoods);
+    	//
+    	
+    	return toResponsSuccess(resultObj);
+    }
+    @ApiOperation(value = "新热门商品信息")
+    @IgnoreAuth
+    @RequestMapping(value = "hotGoods", method = RequestMethod.GET)
+    public Object hotGoods() {
+    	Map<String, Object> resultObj = new HashMap<String, Object>();
+    	//
+    	Map<String, Object> param = new HashMap<String, Object>();
+    	param.put("is_hot", "1");
+        param.put("is_delete", 0);
+        PageHelper.startPage(0, 3,false);
+        List<GoodsVo> hotGoods = goodsService.queryHotGoodsList(param);
+        resultObj.put("hotGoodsList", hotGoods);
+    	//
+    	
+    	return toResponsSuccess(resultObj);
+    }
+    @ApiOperation(value = "topic")
+    @IgnoreAuth
+    @RequestMapping(value = "topic", method = RequestMethod.GET)
+    public Object topic() {
+    	Map<String, Object> resultObj = new HashMap<String, Object>();
+    	//
+    	Map<String, Object> param = new HashMap<String, Object>();
+    	  param.put("offset", 0);
+          param.put("limit", 3);
+          List<TopicVo> topicList = topicService.queryList(param);
+          resultObj.put("topicList", topicList);
+    	//
+    	
+    	return toResponsSuccess(resultObj);
+    }
+    @ApiOperation(value = "brand")
+    @IgnoreAuth
+    @RequestMapping(value = "brand", method = RequestMethod.GET)
+    public Object brand() {
+    	Map<String, Object> resultObj = new HashMap<String, Object>();
+    	//
+    	Map<String, Object> param = new HashMap<String, Object>();
+    	 param.put("is_new", 1);
+         param.put("sidx", "new_sort_order ");
+         param.put("order", "asc ");
+         param.put("offset", 0);
+         param.put("limit", 4);
+         List<BrandVo> brandList = brandService.queryList(param);
+         resultObj.put("brandList", brandList);
+    	//
+    	
+    	return toResponsSuccess(resultObj);
+    }
+    @ApiOperation(value = "category")
+    @IgnoreAuth
+    @RequestMapping(value = "category", method = RequestMethod.GET)
+    public Object category() {
+    	Map<String, Object> resultObj = new HashMap<String, Object>();
+    	//
+    	Map<String, Object> param = new HashMap<String, Object>();
+    	 param = new HashMap<String, Object>();
+         param.put("parent_id", 0);
+         param.put("notName", "推荐");//<>
+         List<CategoryVo> categoryList = categoryService.queryList(param);
+         List< Map<String, Object>> newCategoryList = new ArrayList<>();
+
+         for (CategoryVo categoryItem : categoryList) {
+             param.remove("fields");
+             param.put("parent_id", categoryItem.getId());
+             List<CategoryVo> categoryEntityList = categoryService.queryList(param);
+             List<Integer> childCategoryIds = new ArrayList<>();
+             for (CategoryVo categoryEntity : categoryEntityList) {
+                 childCategoryIds.add(categoryEntity.getId());
+             }
+             //
+             param = new HashMap<String, Object>();
+             param.put("categoryIds", childCategoryIds);
+             param.put("fields", "id as id, name as name, list_pic_url as list_pic_url, retail_price as retail_price");
+             PageHelper.startPage(0, 7,false);
+             List<GoodsVo> categoryGoods = goodsService.queryList(param);
+             Map<String, Object> newCategory = new HashMap<String, Object>();
+             newCategory.put("id", categoryItem.getId());
+             newCategory.put("name", categoryItem.getName());
+             newCategory.put("goodsList", categoryGoods);
+             newCategoryList.add(newCategory);
+         }
+         resultObj.put("categoryList", newCategoryList);
+    	//
+    	
+    	return toResponsSuccess(resultObj);
+    }
+    @ApiOperation(value = "banner")
+    @IgnoreAuth
+    @RequestMapping(value = "banner", method = RequestMethod.GET)
+    public Object banner() {
+    	Map<String, Object> resultObj = new HashMap<String, Object>();
+    	//
+    	Map<String, Object> param = new HashMap<String, Object>();
+    	   param.put("ad_position_id", 1);
+           List<AdVo> banner = adService.queryList(param);
+           resultObj.put("banner", banner);
+    	//
+    	
+    	return toResponsSuccess(resultObj);
+    }
+    @ApiOperation(value = "channel")
+    @IgnoreAuth
+    @RequestMapping(value = "channel", method = RequestMethod.GET)
+    public Object channel() {
+    	Map<String, Object> resultObj = new HashMap<String, Object>();
+    	//
+    	Map<String, Object> param = new HashMap<String, Object>();
+    	 param = new HashMap<String, Object>();
+         param.put("sidx", "sort_order ");
+         param.put("order", "asc ");
+         List<ChannelVo> channel = channelService.queryList(param);
+         resultObj.put("channel", channel);
+    	//
+    	
+    	return toResponsSuccess(resultObj);
+    }
+    
+    /*IndexUrlNewGoods: NewApiRootUrl + 'index/newGoods', //
+    IndexUrlHotGoods: NewApiRootUrl + 'index/hotGoods', //首页数据接口
+    IndexUrlTopic: NewApiRootUrl + 'index/topic', //首页数据接口
+    IndexUrlBrand: NewApiRootUrl + 'index/brand', //首页数据接口IndexUrlChannel
+    IndexUrlCategory: NewApiRootUrl + 'index/category', //首页数据接口IndexUrlChannel
+    IndexUrlBanner: NewApiRootUrl + 'index/banner', //首页数据接口IndexUrlChannel
+    IndexUrlChannel: NewApiRootUrl + 'index/channel', //首页数据接口IndexUrlChannel
+    */
 }
