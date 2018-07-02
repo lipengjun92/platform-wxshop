e0783c9d0e5595d040009dca14b4055395c7ed1e
suzy2 <suzy2@lenovo.com>
2018-07-02 14:33:23 +0800

    分页不再默认count

--- platform-api/src/main/java/com/platform/api/ApiIndexController.java
+++ platform-api/src/main/java/com/platform/api/ApiIndexController.java
@@ -1,24 +1,35 @@
 package com.platform.api;
 
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
-import com.platform.annotation.IgnoreAuth;
-import com.platform.entity.*;
-import com.platform.service.*;
-import com.platform.util.ApiBaseAction;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.pagehelper.PageHelper;
+import com.platform.annotation.IgnoreAuth;
+import com.platform.entity.AdVo;
+import com.platform.entity.BrandVo;
+import com.platform.entity.CartVo;
+import com.platform.entity.CategoryVo;
+import com.platform.entity.ChannelVo;
+import com.platform.entity.GoodsVo;
+import com.platform.entity.TopicVo;
+import com.platform.service.ApiAdService;
+import com.platform.service.ApiBrandService;
+import com.platform.service.ApiCartService;
+import com.platform.service.ApiCategoryService;
+import com.platform.service.ApiChannelService;
+import com.platform.service.ApiGoodsService;
+import com.platform.service.ApiTopicService;
+import com.platform.util.ApiBaseAction;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * 作者: @author Harmon <br>
@@ -43,8 +54,7 @@ public class ApiIndexController extends ApiBaseAction {
     private ApiCategoryService categoryService;
     @Autowired
     private ApiCartService cartService;
-    @Autowired
-    private JedisPool redisService;
+ 
     /**
      * 测试
      */
@@ -61,38 +71,38 @@ public class ApiIndexController extends ApiBaseAction {
     @IgnoreAuth
     @RequestMapping(value = "index", method = RequestMethod.GET)
     public Object index() {
-        Map<String, Object> resultObj = new HashMap();
+        Map<String, Object> resultObj = new HashMap<String, Object>();
         //
-        Map param = new HashMap();
+        Map<String, Object> param = new HashMap<String, Object>();
         param.put("ad_position_id", 1);
         List<AdVo> banner = adService.queryList(param);
         resultObj.put("banner", banner);
         //
-        param = new HashMap();
+        param = new HashMap<String, Object>();
         param.put("sidx", "sort_order ");
         param.put("order", "asc ");
         List<ChannelVo> channel = channelService.queryList(param);
         resultObj.put("channel", channel);
         //
-        param = new HashMap();
+        param = new HashMap<String, Object>();
         param.put("is_new", 1);
         param.put("is_delete", 0);
         param.put("fields", "id, name, list_pic_url, retail_price");
-        PageHelper.startPage(0, 4);
+        PageHelper.startPage(0, 4,false);
         List<GoodsVo> newGoods = goodsService.queryList(param);
         resultObj.put("newGoodsList", newGoods);
         //
-        param = new HashMap();
+        param = new HashMap<String, Object>();
         param.put("is_hot", "1");
         param.put("is_delete", 0);
-        PageHelper.startPage(0, 3);
+        PageHelper.startPage(0, 3,false);
         List<GoodsVo> hotGoods = goodsService.queryHotGoodsList(param);
         resultObj.put("hotGoodsList", hotGoods);
         // 当前购物车中
-        List<CartVo> cartList = new ArrayList();
+        List<CartVo> cartList = new ArrayList<CartVo>();
         if (null != getUserId()) {
             //查询列表数据
-            Map cartParam = new HashMap();
+            Map<String, Object> cartParam = new HashMap<String, Object>();
             cartParam.put("user_id", getUserId());
             cartList = cartService.queryList(cartParam);
         }
@@ -106,7 +116,7 @@ public class ApiIndexController extends ApiBaseAction {
             }
         }
         //
-        param = new HashMap();
+        param = new HashMap<String, Object>();
         param.put("is_new", 1);
         param.put("sidx", "new_sort_order ");
         param.put("order", "asc ");
@@ -115,17 +125,17 @@ public class ApiIndexController extends ApiBaseAction {
         List<BrandVo> brandList = brandService.queryList(param);
         resultObj.put("brandList", brandList);
 
-        param = new HashMap();
+        param = new HashMap<String, Object>();
         param.put("offset", 0);
         param.put("limit", 3);
         List<TopicVo> topicList = topicService.queryList(param);
         resultObj.put("topicList", topicList);
 
-        param = new HashMap();
+        param = new HashMap<String, Object>();
         param.put("parent_id", 0);
         param.put("notName", "推荐");//<>
         List<CategoryVo> categoryList = categoryService.queryList(param);
-        List<Map> newCategoryList = new ArrayList<>();
+        List< Map<String, Object>> newCategoryList = new ArrayList<>();
 
         for (CategoryVo categoryItem : categoryList) {
             param.remove("fields");
@@ -136,12 +146,12 @@ public class ApiIndexController extends ApiBaseAction {
                 childCategoryIds.add(categoryEntity.getId());
             }
             //
-            param = new HashMap();
+            param = new HashMap<String, Object>();
             param.put("categoryIds", childCategoryIds);
             param.put("fields", "id as id, name as name, list_pic_url as list_pic_url, retail_price as retail_price");
-            PageHelper.startPage(0, 7);
+            PageHelper.startPage(0, 7,false);
             List<GoodsVo> categoryGoods = goodsService.queryList(param);
-            Map newCategory = new HashMap();
+            Map<String, Object> newCategory = new HashMap<String, Object>();
             newCategory.put("id", categoryItem.getId());
             newCategory.put("name", categoryItem.getName());
             newCategory.put("goodsList", categoryGoods);
