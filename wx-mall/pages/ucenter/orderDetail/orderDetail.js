24646f566fbe7c3dfa5e7d4254527364bb0657e4
chensf <chensf@chenshangfeng.com>
2018-08-26 05:59:55 +0800

    新增用户收货后手动确认收货功能

--- wx-mall/pages/ucenter/orderDetail/orderDetail.js
+++ wx-mall/pages/ucenter/orderDetail/orderDetail.js
@@ -149,6 +149,89 @@ Page({
     });
 
   },
+  confirmOrder() {
+//确认收货
+      console.log('开始确认收货');
+      let that = this;
+      let orderInfo = that.data.orderInfo;
+      console.log(orderInfo);
+
+      var order_status = orderInfo.order_status;
+      console.log(order_status);
+
+      var errorMessage = '';
+      switch (order_status) {
+          // case 300: {
+          //   console.log('已发货，不能取消');
+          //   errorMessage = '订单已发货';
+          //   break;
+          // }
+          case 301: {
+              console.log('已收货，不能再收货');
+              errorMessage = '订单已收货';
+              break;
+          }
+          case 101: {
+              console.log('已经取消');
+              errorMessage = '订单已取消';
+              break;
+          }
+          case 102: {
+              console.log('已经删除');
+              errorMessage = '订单已删除';
+              break;
+          }
+          case 401: {
+              console.log('已经退款');
+              errorMessage = '订单已退款';
+              break;
+          }
+          case 402: {
+              console.log('已经退款退货');
+              errorMessage = '订单已退货';
+              break;
+          }
+      }
+
+      if (errorMessage != '') {
+          console.log(errorMessage);
+          util.showErrorToast(errorMessage);
+          return false;
+      }
+
+      console.log('可以取消订单的情况');
+      wx.showModal({
+          title: '',
+          content: '确定已经收到商品？',
+          success: function (res) {
+              if (res.confirm) {
+                  console.log('用户点击确定');
+
+                  util.request(api.OrderConfirm, {
+                      orderId: orderInfo.id
+                  }).then(function (res) {
+                      console.log(res.errno);
+                      if (res.errno === 0) {
+                          console.log(res.data);
+                          wx.showModal({
+                              title: '提示',
+                              content: res.data,
+                              showCancel: false,
+                              confirmText: '继续',
+                              success: function (res) {
+                                  //  util.redirect('/pages/ucenter/order/order');
+                                  wx.navigateBack({
+                                      url: 'pages/ucenter/order/order',
+                                  });
+                              }
+                          });
+                      }
+                  });
+
+              }
+          }
+      });
+  },
   onReady: function () {
     // 页面渲染完成
   },
