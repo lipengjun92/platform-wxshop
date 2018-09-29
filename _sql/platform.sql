6e5bb20e270d8b80f5ae5f565b691b5ce07fb039
合肥-传说哥 <939961241@qq.com>
2018-09-29 15:13:44 +0800

    解决微信名存在特殊字符导致的无法登陆问题

--- _sql/platform.sql
+++ _sql/platform.sql
@@ -3891,7 +3891,6 @@ ALTER TABLE nideshop_address ADD is_default INT(1) DEFAULT 0 NULL;
 /** 2018-06-21 会员优惠券添加状态字段 **/
 ALTER TABLE `nideshop_user_coupon` ADD COLUMN `coupon_status` TINYINT (3) UNSIGNED DEFAULT '1' COMMENT '状态 1. 可用 2. 已用 3. 过期';
 /** 2019-07-07 给足迹表添加索引字段 **/
-ALTER TABLE `nideshop_footprint`
-ADD INDEX `index_nideshop_footprint_user_id_goods_id` (`user_id`, `goods_id`) USING BTREE ;
+ALTER TABLE `nideshop_footprint` ADD INDEX `index_nideshop_footprint_user_id_goods_id` (`user_id`, `goods_id`) USING BTREE ;
 /** 2018-09-29 解决微信名存在特殊字符导致的无法登陆问题 **/
 ALTER TABLE nideshop_user CHANGE nickname nickname VARCHAR (60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
