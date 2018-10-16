32aacb19e97e32299721d2d6c5c3d71fc8744d56
合肥-传说哥 <939961241@qq.com>
2018-10-16 09:44:58 +0800

    添加wx.showLoading

--- wx-mall/utils/util.js
+++ wx-mall/utils/util.js
@@ -1,147 +1,150 @@
 var api = require('../config/api.js');
 
 function formatTime(date) {
-  var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var day = date.getDate()
+    var year = date.getFullYear()
+    var month = date.getMonth() + 1
+    var day = date.getDate()
 
-  var hour = date.getHours()
-  var minute = date.getMinutes()
-  var second = date.getSeconds()
+    var hour = date.getHours()
+    var minute = date.getMinutes()
+    var second = date.getSeconds()
 
 
-  return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+    return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 function formatNumber(n) {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+    n = n.toString()
+    return n[1] ? n : '0' + n
 }
 
 /**
  * 封封微信的的request
  */
 function request(url, data = {}, method = "POST", header = "application/x-www-form-urlencoded") {
-  return new Promise(function (resolve, reject) {
-    wx.request({
-      url: url,
-      data: data,
-      method: method,
-      header: {
-        'Content-Type': header,
-        'X-Nideshop-Token': wx.getStorageSync('token')
-      },
-      success: function (res) {
-        console.log("success");
+    wx.showLoading({
+        title: '加载中...',
+    });
+    return new Promise(function (resolve, reject) {
+        wx.request({
+            url: url,
+            data: data,
+            method: method,
+            header: {
+                'Content-Type': header,
+                'X-Nideshop-Token': wx.getStorageSync('token')
+            },
+            success: function (res) {
+                console.log("success");
+                wx.hideLoading();
+                if (res.statusCode == 200) {
 
-        if (res.statusCode == 200) {
+                    if (res.data.errno == 401) {
+                        //需要登录后才可以操作
+                        wx.showModal({
+                            title: '',
+                            content: '请先登录',
+                            success: function (res) {
+                                if (res.confirm) {
+                                    wx.removeStorageSync("userInfo");
+                                    wx.removeStorageSync("token");
 
-          if (res.data.errno == 401) {
-            //需要登录后才可以操作
-            wx.showModal({
-                title: '',
-                content: '请先登录',
-                success: function (res){
-                    if (res.confirm) {
-                        wx.removeStorageSync("userInfo");
-                        wx.removeStorageSync("token");
-                        
-                        wx.switchTab({
-                            url: '/pages/ucenter/index/index'
+                                    wx.switchTab({
+                                        url: '/pages/ucenter/index/index'
+                                    });
+                                }
+                            }
                         });
+                    } else {
+                        resolve(res.data);
                     }
+                } else {
+                    reject(res.errMsg);
                 }
-            });
-          } else {
-            resolve(res.data);
-          }
-        } else {
-          reject(res.errMsg);
-        }
 
-      },
-      fail: function (err) {
-        reject(err)
-        console.log("failed")
-      }
-    })
-  });
+            },
+            fail: function (err) {
+                reject(err)
+                console.log("failed")
+            }
+        })
+    });
 }
 
 /**
  * 检查微信会话是否过期
  */
 function checkSession() {
-  return new Promise(function (resolve, reject) {
-    wx.checkSession({
-      success: function () {
-        resolve(true);
-      },
-      fail: function () {
-        reject(false);
-      }
-    })
-  });
+    return new Promise(function (resolve, reject) {
+        wx.checkSession({
+            success: function () {
+                resolve(true);
+            },
+            fail: function () {
+                reject(false);
+            }
+        })
+    });
 }
 
 /**
  * 调用微信登录
  */
 function login() {
-  return new Promise(function (resolve, reject) {
-    wx.login({
-      success: function (res) {
-        if (res.code) {
-          //登录远程服务器
-          console.log(res)
-          resolve(res);
-        } else {
-          reject(res);
-        }
-      },
-      fail: function (err) {
-        reject(err);
-      }
+    return new Promise(function (resolve, reject) {
+        wx.login({
+            success: function (res) {
+                if (res.code) {
+                    //登录远程服务器
+                    console.log(res)
+                    resolve(res);
+                } else {
+                    reject(res);
+                }
+            },
+            fail: function (err) {
+                reject(err);
+            }
+        });
     });
-  });
 }
 
 function redirect(url) {
 
-  //判断页面是否需要登录
-  if (false) {
-    wx.redirectTo({
-      url: '/pages/auth/login/login'
-    });
-    return false;
-  } else {
-    wx.redirectTo({
-      url: url
-    });
-  }
+    //判断页面是否需要登录
+    if (false) {
+        wx.redirectTo({
+            url: '/pages/auth/login/login'
+        });
+        return false;
+    } else {
+        wx.redirectTo({
+            url: url
+        });
+    }
 }
 
 function showErrorToast(msg) {
-  wx.showToast({
-    title: msg,
-    image: '/static/images/icon_error.png'
-  })
+    wx.showToast({
+        title: msg,
+        image: '/static/images/icon_error.png'
+    })
 }
 
 function showSuccessToast(msg) {
-  wx.showToast({
-    title: msg,
-  })
+    wx.showToast({
+        title: msg,
+    })
 }
 
 module.exports = {
-  formatTime,
-  request,
-  redirect,
-  showErrorToast,
-  showSuccessToast,
-  checkSession,
-  login,
+    formatTime,
+    request,
+    redirect,
+    showErrorToast,
+    showSuccessToast,
+    checkSession,
+    login,
 }
 
 
