e33c00bd5418731d0623a2808307166398f0a828
合肥-传说哥 <939961241@qq.com>
2018-10-17 14:39:43 +0800

    添加tomcat7插件，添加tomcat7启动脚本

---
+++ bin/tomcat7.bat
@@ -0,0 +1,21 @@
+@echo off
+
+for /f "tokens=3,4" %%a in ('"reg query HKEY_CLASSES_ROOT\http\shell\open\command"') do (set SoftWareRoot=%%a %%b)
+start "" % SoftWareRoot % "http://localhost:8080/platform"
+
+title %cd%
+echo.
+echo [info] Use Tomcat7 plugin run
+echo.
+rem pause
+rem echo.
+
+cd %~dp0
+cd..
+
+set MAVEN_OPTS=%MAVEN_OPTS% -Xms256m -Xmx512m -XX:PermSize=128m -XX:MaxPermSize=256m
+call mvn tomcat7:run
+
+cd bin
+
+pause
