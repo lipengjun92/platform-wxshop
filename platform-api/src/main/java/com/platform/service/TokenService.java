11e5843ce6aa288f53a7c0040a1d400462ef27a9
合肥-传说哥 <939961241@qq.com>
2019-05-27 14:37:46 +0800

    调整token 30天过期，退款bug修复

--- platform-api/src/main/java/com/platform/service/TokenService.java
+++ platform-api/src/main/java/com/platform/service/TokenService.java
@@ -15,8 +15,8 @@ import java.util.Map;
 public class TokenService {
     @Autowired
     private ApiTokenMapper tokenDao;
-    //12小时后过期
-    private final static int EXPIRE = 3600 * 12;
+    //30天过期
+    private final static int EXPIRE = 3600 * 24 * 30;
 
     public TokenEntity queryByUserId(Long userId) {
         return tokenDao.queryByUserId(userId);
