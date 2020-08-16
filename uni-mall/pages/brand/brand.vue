fb5ec435377e10a0c0536ed8ed4bdc2825f8dca2
lipengjun <pengjun92>
2020-08-16 18:32:18 +0800

    品牌制造商及详情页面

--- uni-mall/pages/brand/brand.vue
+++ uni-mall/pages/brand/brand.vue
@@ -1,21 +1,114 @@
 <template>
-	<view>
+	<view class="container">
+		<view class="brand-list">
+			<navigator :url="'../brandDetail/brandDetail?id='+item.id" class="item" v-for="(item, index) in brandList" :key="index">
+				<view class="img-bg">
+					<image :src="item.app_list_pic_url" background-size="cover"></image>
+				</view>
+				<view class="txt-box">
+					<view class="line">
+						<text class="name">{{item.name||''}}</text>
+						<text class="s">|</text>
+						<text class="price">{{item.floor_price||0}}元起</text>
+					</view>
+				</view>
+			</navigator>
+		</view>
 	</view>
 </template>
 
 <script>
-	const util = require("@/utils/util.js")
+	const util = require("@/utils/util.js");
+	const api = require('@/utils/api.js');
 	export default {
 		data() {
 			return {
+				brandList: [],
+				page: 1,
+				size: 10,
+				totalPages: 1
 			}
 		},
 		methods: {
+			getBrandList: function() {
+				let that = this;
+				util.request(api.BrandList, {
+					page: that.page,
+					size: that.size
+				}).then(function(res) {
+					if (res.errno === 0) {
+						that.brandList = that.brandList.concat(res.data.data)
+						that.totalPages = res.data.totalPages
+					}
+				});
+			}
+		},
+		onReachBottom() {
+			if (this.totalPages > this.page) {
+				this.page = this.page + 1
+			} else {
+				return false;
+			}
+
+			this.getBrandList();
 		},
 		onLoad: function() {
+			this.getBrandList();
 		}
 	}
 </script>
 
 <style lang="scss">
+	.brand-list .item {
+		display: block;
+		width: 750rpx;
+		height: 416rpx;
+		position: relative;
+		margin-bottom: 4rpx;
+	}
+
+	.brand-list .item .img-bg {
+		position: absolute;
+		left: 0;
+		top: 0;
+		z-index: 0;
+		width: 750rpx;
+		height: 417rpx;
+		overflow: hidden;
+	}
+
+	.brand-list .item .img-bg image {
+		width: 750rpx;
+		height: 416rpx;
+	}
+
+	.brand-list .item .txt-box {
+		position: absolute;
+		left: 0;
+		top: 0;
+		display: table;
+		z-index: 0;
+		width: 750rpx;
+		height: 417rpx;
+	}
+
+	.brand-list .item .line {
+		display: table-cell;
+		vertical-align: middle;
+		text-align: center;
+		height: 63rpx;
+		line-height: 63rpx;
+	}
+
+	.brand-list .item .line text {
+		font-size: 35rpx;
+		font-weight: 700;
+		text-shadow: 1rpx 1rpx rgba(0, 0, 0, .32);
+		color: #fff;
+	}
+
+	.brand-list .item .line .s {
+		padding: 0 10rpx;
+		font-size: 40rpx;
+	}
 </style>
--- uni-mall/pages/brandDetail/brandDetail.vue
+++ uni-mall/pages/brandDetail/brandDetail.vue
@@ -1,21 +1,193 @@
 <template>
-	<view>
+	<view class="container">
+		<view class="brand-info">
+			<view class="name">
+				<image class="img" :src="brand.app_list_pic_url" background-size="cover"></image>
+				<view class="info-box">
+					<view class="info">
+						<text class="txt">{{brand.name||''}}</text>
+						<text class="line"></text>
+					</view>
+				</view>
+			</view>
+			<view class="desc">
+				{{brand.simple_desc}}
+			</view>
+		</view>
+
+		<view class="cate-item">
+			<view class="b">
+				<block v-for="(iitem, iindex) in goodsList" :key="iindex">
+					<navigator :class="'item ' + (iindex % 2 == 0 ? 'item-b' : '')" :url="'../goods/goods?id='+iitem.id">
+						<image class="img" :src="iitem.list_pic_url" background-size="cover"></image>
+						<text class="name">{{iitem.name||''}}</text>
+						<text class="price">￥{{iitem.retail_price||0}}</text>
+					</navigator>
+				</block>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	const util = require("@/utils/util.js")
+	const util = require("@/utils/util.js");
+	const api = require('@/utils/api.js');
 	export default {
 		data() {
 			return {
+				id: 0,
+				brand: {},
+				goodsList: [],
+				page: 1,
+				size: 1000
 			}
 		},
 		methods: {
+			getBrand: function() {
+				let that = this;
+				util.request(api.BrandDetail, {
+					id: that.id
+				}).then(function(res) {
+					if (res.errno === 0) {
+						that.brand = res.data.brand
+
+						that.getGoodsList();
+					}
+				});
+			},
+			getGoodsList() {
+				var that = this;
+
+				util.request(api.GoodsList, {
+					brandId: that.id,
+					page: that.page,
+					size: that.size
+				}).then(function(res) {
+					if (res.errno === 0) {
+						that.goodsList = res.data.goodsList;
+					}
+				});
+			}
 		},
-		onLoad: function() {
+		onLoad: function(options) {
+			// 页面初始化 options为页面跳转所带来的参数
+			var that = this;
+			that.id = parseInt(options.id);
+			this.getBrand();
 		}
 	}
 </script>
 
 <style lang="scss">
+	page {
+		background: #f4f4f4;
+	}
+
+	.brand-info .name {
+		width: 100%;
+		height: 290rpx;
+		position: relative;
+	}
+
+	.brand-info .img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 290rpx;
+	}
+
+	.brand-info .info-box {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 290rpx;
+		text-align: center;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.brand-info .info {
+		display: block;
+	}
+
+	.brand-info .txt {
+		display: block;
+		height: 37.5rpx;
+		font-size: 37.5rpx;
+		color: #fff;
+	}
+
+	.brand-info .line {
+		margin: 0 auto;
+		margin-top: 16rpx;
+		display: block;
+		height: 2rpx;
+		width: 145rpx;
+		background: #fff;
+	}
+
+	.brand-info .desc {
+		background: #fff;
+		width: 100%;
+		height: auto;
+		overflow: hidden;
+		padding: 41.5rpx 31.25rpx;
+		font-size: 30rpx;
+		color: #666;
+		line-height: 41.5rpx;
+		text-align: center;
+	}
+
+	.cate-item .b {
+		width: 750rpx;
+		height: auto;
+		overflow: hidden;
+		border-top: 1rpx solid #f4f4f4;
+		margin-top: 20rpx;
+	}
+
+	.cate-item .b .item {
+		float: left;
+		background: #fff;
+		width: 375rpx;
+		padding-bottom: 33.333rpx;
+		border-bottom: 1rpx solid #f4f4f4;
+		height: auto;
+		overflow: hidden;
+		text-align: center;
+	}
+
+	.cate-item .b .item-b {
+		border-right: 1rpx solid #f4f4f4;
+	}
+
+	.cate-item .item .img {
+		margin-top: 10rpx;
+		width: 302rpx;
+		height: 302rpx;
+	}
+
+	.cate-item .item .name {
+		display: block;
+		width: 365.625rpx;
+		height: 35rpx;
+		padding: 0 20rpx;
+		overflow: hidden;
+		margin: 11.5rpx 0 22rpx 0;
+		text-align: center;
+		font-size: 30rpx;
+		color: #333;
+	}
+
+	.cate-item .item .price {
+		display: block;
+		width: 365.625rpx;
+		height: 30rpx;
+		text-align: center;
+		font-size: 30rpx;
+		color: #b4282d;
+	}
 </style>
