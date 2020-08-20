01906f5d84c30c589f038371760c91d931ea9da4
lipengjun <pengjun92>
2020-08-20 13:35:46 +0800

    1下单页面开发；2选择优惠券页面开发；3我的订单页面开发；4收货地址页面开发；5支付package不匹配修复；6支付结果页面bug修复；7取消支付后再次支付提示“订单已支付，请不要重复操作”bug修复

--- uni-mall/pages/payResult/payResult.vue
+++ uni-mall/pages/payResult/payResult.vue
@@ -20,7 +20,7 @@
 				</view>
 				<view class="btns">
 					<navigator class="btn" url="/pages/ucenter/order/order" open-type="redirect">查看订单</navigator>
-					<view class="btn" bindtap='payOrder'>重新付款</view>
+					<view class="btn" @tap='payOrder'>重新付款</view>
 				</view>
 			</view>
 		</view>
@@ -28,7 +28,7 @@
 </template>
 
 <script>
-	const util = require("@/utils/util.js")
+	const util = require("@/utils/util.js");
 	const api = require('@/utils/api.js');
 	export default {
 		data() {
@@ -41,14 +41,14 @@
 			updateSuccess: function() {
 				let that = this
 				util.request(api.OrderQuery, {
-					orderId: that.data.orderId
+					orderId: that.orderId
 				}).then(function(res) {})
 			},
 			payOrder() {
-				pay.payOrder(parseInt(this.data.orderId)).then(res => {
-					this.status = true
+				util.payOrder(parseInt(this.orderId)).then(res => {
+					that.status = true
 				}).catch(res => {
-					util.showErrorToast('支付失败');
+					util.toast('支付失败');
 				});
 			}
 		},
@@ -56,7 +56,6 @@
 			// 页面初始化 options为页面跳转所带来的参数
 			this.orderId = options.orderId || 24
 			this.status = options.status
-			this.updateSuccess()
 		}
 	}
 </script>
@@ -79,9 +78,9 @@
 
 	.pay-result .msg {
 		text-align: center;
-		margin: 100 rpx auto;
+		margin: 100rpx auto;
 		color: #2bab25;
-		font-size: 36 rpx;
+		font-size: 36rpx;
 	}
 
 	.pay-result .btns {
@@ -92,33 +91,33 @@
 
 	.pay-result .btn {
 		text-align: center;
-		height: 80 rpx;
-		margin: 0 20 rpx;
-		width: 200 rpx;
-		line-height: 78 rpx;
+		height: 80rpx;
+		margin: 0 20rpx;
+		width: 200rpx;
+		line-height: 78rpx;
 		border: 1px solid #868686;
 		color: #000000;
-		border-radius: 5 rpx;
+		border-radius: 5rpx;
 	}
 
 	.pay-result .error .msg {
 		color: #b4282d;
-		margin-bottom: 60 rpx;
+		margin-bottom: 60rpx;
 	}
 
 	.pay-result .error .tips {
 		color: #7f7f7f;
-		margin-bottom: 70 rpx;
+		margin-bottom: 70rpx;
 	}
 
 	.pay-result .error .tips .p {
-		font-size: 24 rpx;
-		line-height: 42 rpx;
+		font-size: 24rpx;
+		line-height: 42rpx;
 		text-align: center;
 	}
 
 	.pay-result .error .tips .p {
-		line-height: 42 rpx;
+		line-height: 42rpx;
 		text-align: center;
 	}
 </style>
