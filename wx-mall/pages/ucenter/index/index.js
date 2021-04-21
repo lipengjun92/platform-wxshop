74480428b38bcfcdebe9cee34abe2ac4217ef439
lipengjun <pengjun92>
2021-04-21 20:16:11 +0800

    小程序登录、用户信息相关接口调整说明wx.getUserProfile替换wx.getUserInfo

--- wx-mall/pages/ucenter/index/index.js
+++ wx-mall/pages/ucenter/index/index.js
@@ -5,11 +5,17 @@ var app = getApp();
 
 Page({
     data: {
+        canIUseGetUserProfile: false,
         userInfo: {},
         hasMobile: ''
     },
     onLoad: function (options) {
         // 页面初始化 options为页面跳转所带来的参数
+        if (wx.getUserProfile) {
+            this.setData({
+                canIUseGetUserProfile: true
+            })
+        }
     },
     onReady: function () {
 
@@ -37,49 +43,41 @@ Page({
     onUnload: function () {
         // 页面关闭
     },
-    bindGetUserInfo(e) {
-      let userInfo = wx.getStorageSync('userInfo');
-      let token = wx.getStorageSync('token');
-      if (userInfo && token) {
-        return;
-      }
-        if (e.detail.userInfo){
-            //用户按了允许授权按钮
-            user.loginByWeixin(e.detail).then(res => {
-                this.setData({
-                    userInfo: res.data.userInfo
+    getUserProfile() {
+        // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+        // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+        wx.getUserProfile({
+            desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+            success: (resp) => {
+                //登录远程服务器
+                user.loginByWeixin(resp).then(res => {
+                    this.setData({
+                        userInfo: res.data.userInfo
+                    });
+                    app.globalData.userInfo = res.data.userInfo;
+                    app.globalData.token = res.data.token;
+                }).catch((err) => {
+                    console.log(err)
                 });
-                app.globalData.userInfo = res.data.userInfo;
-                app.globalData.token = res.data.token;
-            }).catch((err) => {
-                console.log(err)
-            });
-        } else {
-            //用户按了拒绝按钮
-            wx.showModal({
-                title: '警告通知',
-                content: '您点击了拒绝授权,将无法正常显示个人信息,点击确定重新获取授权。',
-                success: function (res) {
-                    if (res.confirm) {
-                        wx.openSetting({
-                            success: (res) => {
-                                if (res.authSetting["scope.userInfo"]) {////如果用户重新同意了授权登录
-                                    user.loginByWeixin(e.detail).then(res => {
-                                        this.setData({
-                                            userInfo: res.data.userInfo
-                                        });
-                                        app.globalData.userInfo = res.data.userInfo;
-                                        app.globalData.token = res.data.token;
-                                    }).catch((err) => {
-                                        console.log(err)
-                                    });
-                                }
-                            }
-                        })
-                    }
-                }
-            });
+            }
+        })
+    },
+    bindGetUserInfo(e) {
+        let userInfo = wx.getStorageSync('userInfo');
+        let token = wx.getStorageSync('token');
+        if (userInfo && token) {
+            return;
         }
+        //用户按了允许授权按钮
+        user.loginByWeixin(e.detail).then(res => {
+            this.setData({
+                userInfo: res.data.userInfo
+            });
+            app.globalData.userInfo = res.data.userInfo;
+            app.globalData.token = res.data.token;
+        }).catch((err) => {
+            console.log(err)
+        });
     },
     exitLogin: function () {
         wx.showModal({
