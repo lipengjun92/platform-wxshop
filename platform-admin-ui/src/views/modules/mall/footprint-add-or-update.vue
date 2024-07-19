96390eff6a3f8d1185f0dcac9056c86ac68e9801
李鹏军 <pengjun.9>
2024-07-19 21:25:53 +0800

    Springboot版本initial

---
+++ platform-admin-ui/src/views/modules/mall/footprint-add-or-update.vue
@@ -0,0 +1,91 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : !disabled ? '修改' : '查看'"
+    :close-on-click-modal="false"
+    :lock-scroll="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+      <el-form-item label="会员Id" prop="userId">
+        <el-input v-model="dataForm.userId" :disabled="disabled" placeholder="会员Id"></el-input>
+      </el-form-item>
+      <el-form-item label="商品id" prop="goodsId">
+        <el-input v-model="dataForm.goodsId" :disabled="disabled" placeholder="商品id"></el-input>
+      </el-form-item>
+      <el-form-item label="记录时间" prop="addTime">
+        <el-input v-model="dataForm.addTime" :disabled="disabled" placeholder="记录时间"></el-input>
+      </el-form-item>
+      <el-form-item label="转发人" prop="referrer">
+        <el-input v-model="dataForm.referrer" :disabled="disabled" placeholder="转发人"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button v-if="!disabled" type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        disabled: false,
+        visible: false,
+        dataForm: {
+          id: '',
+          userId: '',
+          goodsId: '',
+          addTime: '',
+          referrer: ''},
+        dataRule: {
+          name: [
+            {required: true, message: '名称不能为空', trigger: 'blur'}
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id, disabled) {
+        this.disabled = disabled
+        this.dataForm.id = id || ''
+        this.visible = true
+        this.$nextTick(() => {
+          this.$refs['dataForm'].resetFields()
+          if (this.dataForm.id) {
+            this.$http({
+              url: `/mall/footprint/info/${this.dataForm.id}`,
+              method: 'get'
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.dataForm = data.data
+              }
+            })
+          }
+        })
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm']
+          .validate((valid) => {
+            if (valid) {
+              this.$http({
+                url: `/mall/footprint/${!this.dataForm.id ? 'save' : 'update'}`,
+                method: 'post',
+                data: this.dataForm
+              }).then(({data}) => {
+                if (data && data.code === 0) {
+                  this.$message({
+                    message: '操作成功',
+                    type: 'success',
+                    duration: 1500
+                  })
+                  this.visible = false
+                  this.$emit('refreshDataList')
+                }
+              })
+            }
+          })
+      }
+    }
+  }
+</script>
